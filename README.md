3271d7985d9f69f3492e8a94a3689fae52854fe7
Tomasz Tarnowski <tomasz.tarnowski@thirdbridge.com>
2021-01-18 22:08:24 +0000

    Day1 Code: Blinking built-in diode.

---
+++ README.md
@@ -0,0 +1,38 @@
+# 100DaysOfCode in IoT Challenge Day 1
+## Hello World
+
+Blinking ESP8266 built-in diode.
+
+## Requirements
+1. Visual Studio Code IDE:
+https://code.visualstudio.com/
+2. C/C++ VSCode extension:
+https://marketplace.visualstudio.com/items?itemName=ms-vscode.cpptools
+3. Clang-Format VSCode extension:
+https://marketplace.visualstudio.com/items?itemName=xaver.clang-format
+4. PlatformIO VSCode extension:
+https://marketplace.visualstudio.com/items?itemName=platformio.platformio-ide
+
+## Building 
+In Visual Studio Code go to:
+
+`View -> Command Pallette...`
+
+Type `PlatformIO: Build`
+
+Press Enter
+
+## Deploying code to ESP8266
+Connect ESP8266 to your computer
+
+In Visual Studio Code go to:
+
+`View -> Command Pallette...`
+
+Type `PlatformIO: Upload`
+
+Press Enter
+
+## Link to other days:
+
+https://github.com/ttarnowski/100DaysOfCodeInIoT
--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,15 @@
 #include <Arduino.h>
 
-// H . . . .
-// E .
-// L . - . . 
-// O - - -
-
-// W . - - 
-// R . - .
-// L . - . .
-// D - . .
-
-
-void dot(bool isLast = false) {
-  digitalWrite(D1, HIGH);
-  delay(250);
-  digitalWrite(D1, LOW);
-  delay(isLast ? 750 : 250);
-}
-
-void dash(bool isLast = false) {
-  digitalWrite(D1, HIGH);
-  delay(600);
-  digitalWrite(D1, LOW);
-  delay(isLast ? 750 : 250);
-}
-
-void h() {
-  dot();
-  dot();
-  dot();
-  dot(true);
-}
-
-void e() {
-  dot(true);
-}
-
-void l() {
-  dot();
-  dash();
-  dot();
-  dot(true);
-}
-
-void o() {
-  dash();
-  dash();
-  dash(true);
-}
-
-void w() {
-  dot();
-  dash();
-  dash(true);
-}
-
-void r() {
-  dot();
-  dash();
-  dot(true);
-}
-
-void d() {
-  dash();
-  dot();
-  dot(true);
-}
-
 void setup() {
   Serial.begin(115200);
-  pinMode(D1, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  h();
-  e();
-  l();
-  l();
-  o();
+  Serial.println("Low Voltage");
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
-  w();
-  o();
-  r();
-  l();
-  d();  
-  Serial.println("Hello World");
-  delay(2000);
+  Serial.println("High Voltage");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
 }
